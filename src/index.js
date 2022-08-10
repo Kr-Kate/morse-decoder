e125ac529030b0dbfe298930f02f98a322198c55
Kr-Kate <loly1995pop@gmail.com>
2022-08-10 17:56:39 +0400

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let codeMorze = '';
+    let result = '';
+    let j = 9;
+    for(let i = 0; i < expr.length; i++) {
+        if(i % 2 === 0) {
+            if(expr[i] == 1) {
+                if(expr[i + 1] == 1) {
+                    codeMorze = codeMorze + '-';
+                }
+                else{
+                    codeMorze = codeMorze + '.';
+                }
+            }
+            else if(expr[i] == '*') {
+                result = result + ' ';
+                i += 10;
+                j += 10;
+            }
+        }
+        if(i === j) {
+                result = result + MORSE_TABLE[codeMorze];
+                codeMorze = '';
+                j += 10;
+            }
+    }
+    return result;
 }
 
 module.exports = {
